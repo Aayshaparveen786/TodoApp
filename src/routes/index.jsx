3f3b359295dfbd98265cae4e1826a114131773b0
Aayshaparveen <aaysha.parveen@ismartapps.co.uk>
2023-07-17 14:22:57 +0530

    add

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import React from "react";
 import Other from "../components/Other";
 import Home from "../Home/Home";
 const MainFram = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
         <Route path="/Other" element={<Other />} />
       </Routes>
     </>
